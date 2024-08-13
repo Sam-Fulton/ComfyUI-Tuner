01acf4f8024703583b725d8faa36238540837ac8
Sam-Fulton <sam.fulton105@gmail.com>
2024-08-13 17:02:03 +0100

    basic css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './App.css';
 import UploadWorkflowPage from './pages/UploadWorkflowPage';
 import CategorisationPage from './pages/CategorisationPage';
 import RunPage from './pages/RunPage';
